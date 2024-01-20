cb31959c6269f28de6d1c26a40a7bfcd0b1546a0
luvsseul <150340509+luvsseul@users.noreply.github.com>
2024-01-20 21:29:10 +0900

    OtherComponent

--- src/components/page/item/image.ts
+++ src/components/page/item/image.ts
@@ -4,14 +4,14 @@ export class ImageComponent extends BaseComponent<HTMLElement> {
     constructor(title: string, url: string) {
         super(`<section class="image">
         <div class="image__holder"><img class="image__thumbnail"></div>
-        <p class="image__title"></p>
+        <h2 class="image__title"></h2>
     </section>`);
-         
+
+    const titleElement = this.element.querySelector('.image__title')! as HTMLHeadingElement;
+    titleElement.textContent = title;
+
     const imageElement = this.element.querySelector('.image__thumbnail')! as HTMLImageElement;
     imageElement.src = url;
     imageElement.alt = title;
-
-    const titleElement = this.element.querySelector('.image__title')! as HTMLParagraphElement;
-    titleElement.textContent = title;
     }
 }
