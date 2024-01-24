2aca0fb235fb3c257944a5cd6d37219c36df7fb1
luvsseul <150340509+luvsseul@users.noreply.github.com>
2024-01-24 15:52:23 +0900

    CloseBtn

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -4,7 +4,12 @@ import { BaseComponent, Component } from '../component.js';
 export interface Composable {
     addChild(child: Component): void;
 }
+
+type OnCloseListener = () => void;
+
 class PageItemComponent extends BaseComponent<HTMLElement> implements Composable {
+    private closeListener?: OnCloseListener;
+
     constructor() {
         super(`<li class="page-item">
         <section class="page-item__body"></section>
@@ -13,10 +18,19 @@ class PageItemComponent extends BaseComponent<HTMLElement> implements Composable
         </div>
         </li>
     `);
+    const closeBtn = this.element.querySelector('.close')! as HTMLElement;
+    closeBtn.onclick = () => {
+        this.closeListener && this.closeListener();
+    }    
     }
+
     addChild(child: Component) {
     const container = this.element.querySelector('.page-item__body')! as HTMLElement;
     child.attachTo(container);
+    }
+
+    setOnCloseListener(listener: OnCloseListener) {
+        this.closeListener = listener;
     }
 }
 export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable {
@@ -27,5 +41,8 @@ export class PageComponent extends BaseComponent<HTMLUListElement> implements Co
         const item = new PageItemComponent();
         item.addChild(section);
         item.attachTo(this.element, 'beforeend');
+        item.setOnCloseListener(()=>{
+            item.removeFrom(this.element);
+        });
     }
 }
