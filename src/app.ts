ef627980ed0b405f255d531cf2b99bf8bd897cfa
luvsseul <150340509+luvsseul@users.noreply.github.com>
2024-01-24 13:45:22 +0900

    PageItemComponent

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,27 @@
+import { Component } from './components/component.js';
 import { ImageComponent } from './components/page/item/image.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { VideoComponent } from './components/page/item/video.js';
-import { PageComponent } from './components/page/page.js';
+import { Composable, PageComponent } from './components/page/page.js';
 
 class App {
-    private readonly page: PageComponent;
+    private readonly page: Component & Composable;
     constructor(appRoot: HTMLElement){
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
 
         const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
-        image.attachTo(appRoot, 'beforeend');
+        this.page.addChild(image);
         
         const note = new NoteComponent('Note Title', 'Note body');
-        note.attachTo(appRoot, 'beforeend');      
+        this.page.addChild(note);
 
         const todo = new TodoComponent('Todo Title', 'Study Hard!');
-        todo.attachTo(appRoot, 'beforeend');  
+        this.page.addChild(todo);
 
         const video = new VideoComponent('Video Title','https://www.youtube.com/embed/zkP6LfeJBmw');
-        video.attachTo(appRoot, 'beforeend');
+        this.page.addChild(video);
     }
 }
 new App(document.querySelector('.document')! as HTMLElement);
